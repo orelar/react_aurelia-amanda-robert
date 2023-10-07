3f99fdfdf2d46c82c986c5d1d10aa3f504ba03f3
Aurelia Amanda Robert <aurelia.amanda@ui.ac.id>
2023-10-08 02:23:58 +0800

    Finish do all assignments, summary and screenshots for Introduction REST API

---
+++ 21_introduction-restfull-api/praktikum/src/components/login/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Login() {
+  return (
+    <><div>Login</div><Link to="/">Go to Home Page</Link></>
+  )
+}
+
+export default Login
